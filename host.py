88087b5afeb729f6f6690fb6f84acb66240b6b24
BelusKirill <belus@vikab.by>
2022-12-06 23:55:56 +0300

    add get_questions_ru, get_questions_en

--- host.py
+++ host.py
@@ -3,12 +3,36 @@ from flask_cors import CORS
 from gevent.pywsgi import WSGIServer
 from config import getData
 
+import sqlite3
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.route('/get_questions_ru')
+def get_questions_ru():
+    res = {}
+    con = sqlite3.connect('./db/example.sqlite')
+
+    with con:
+        data = con.execute("SELECT id, id_survey, question_ru FROM questions")
+        for index, row in enumerate(data):
+            res[index] = row
+
+    print(res)
+    return res
+
+@app.route('/get_questions_en')
+def get_questions_en():
+    res = {}
+    con = sqlite3.connect('./db/example.sqlite')
+
+    with con:
+        data = con.execute("SELECT id, id_survey, question_en FROM questions")
+        for index, row in enumerate(data):
+            res[index] = row
+
+    print(res)
+    return res
 
 if __name__ == '__main__':
     ip = getData('host', 'ip')
